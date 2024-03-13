ed78371283e912d99042c53f031fad353403ceed
bi305 <razab0715@gmail.com>
2024-03-14 04:28:18 +0500

    test  accounts

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar({ className }: { className?: string }) {
             </div>
           </button>
           <button className="p-[3px] relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-indigo-600 to-purple-500 rounded-lg" />
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
             <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
               <Link href="https://github.com/bi305" target="_blank">
                 Github
