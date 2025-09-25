4c8223fd0ea8ad5827bb7c5727fecdb70dc442bf
Bilal's <bilal@rulrr.com>
2025-09-25 14:41:44 +0500

    add global types

---
+++ app/types/react-vertical-timeline-component.d.ts
@@ -0,0 +1,23 @@
+declare module "react-vertical-timeline-component" {
+  import * as React from "react";
+
+  export interface VerticalTimelineProps {
+    animate?: boolean;
+    className?: string;
+    layout?: "1-column" | "2-columns";
+  }
+
+  export interface VerticalTimelineElementProps {
+    className?: string;
+    contentStyle?: React.CSSProperties;
+    contentArrowStyle?: React.CSSProperties;
+    date?: string | React.ReactNode;
+    icon?: React.ReactNode;
+    iconStyle?: React.CSSProperties;
+    position?: "left" | "right";
+    visible?: boolean;
+  }
+
+  export const VerticalTimeline: React.FC<VerticalTimelineProps>;
+  export const VerticalTimelineElement: React.FC<VerticalTimelineElementProps>;
+}
